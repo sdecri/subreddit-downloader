d133856ea3644c4160b5e955502e23b3a9115c58
sdecri <simone.decristofaro85@gmail.com>
2021-06-29 12:08:19 +0200

    Prepared command line parameters

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import logging
 from logging import config
 
 import time, math, inspect
+import praw
 
 # INIT LOG <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 import PARAMETER
@@ -17,6 +18,26 @@ logging.config.fileConfig(LOGGING_CONFIG_FILE_PATH)
 
 # logging.basicConfig()
 log_console = logging.getLogger('onlyconsole')
+log = logging.getLogger()
+
+PARAMETER_REDDIT_CLIENT_ID = "reddit_client_id"
+PARAMETER_REDDIT_USERNAME = "reddit_username"
+PARAMETER_REDDIT_SECRET = "reddit_secret"
+
+
+def get_long_option(parameter_name):
+    return '--{}'.format(parameter_name)
+
+
+def get_option_declaration(parameter_name):
+    return '{}='.format(parameter_name)
+
+
+def check_existence_required_parameter(parameter_name, parameter_value):
+    if parameter_value is None:
+        print("No value specified for required parameter {}".format(parameter_name))
+        print(__help())
+        sys.exit(2)
 
 
 def main(argv):
@@ -29,73 +50,71 @@ def parse_command_line(argv):
     try:
         opts, args = getopt.getopt(argv, "ht:f:c:", [
             "help"
-            , "type="
-            , "file-path="
-            , "chunk-size="
-            , "table-prefix="
-            , "dbhost="
-            , "dbport="
-            , "dbname="
-            , "dbuser="
-            , "dbpassword="
-            , "drop-table="
+            , get_option_declaration(PARAMETER_REDDIT_USERNAME)
+            , get_option_declaration(PARAMETER_REDDIT_CLIENT_ID)
+            , get_option_declaration(PARAMETER_REDDIT_SECRET)
+            # , "chunk-size="
+            # , "table-prefix="
+            # , "dbhost="
+            # , "dbport="
+            # , "dbname="
+            # , "dbuser="
+            # , "dbpassword="
+            # , "drop-table="
         ])
     except getopt.GetoptError:
-        log_console.exception("ERROR parsing command line arguments")
+        log.exception("ERROR parsing command line arguments")
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print(__help())
             sys.exit()
-        elif opt in ("-t", "--run-type"):
-            PARAMETER.TYPE = arg
-        elif opt in ("-f", "--file-path"):
-            PARAMETER.FILE_PATH = os.path.abspath(arg)
-        elif opt in ("-c", "--chunk-size"):
-            PARAMETER.CHUNK_SIZE = int(arg)
-        elif opt == "--table-prefix":
-            PARAMETER.TABLE_PREFIX = arg
-        elif opt == "--dbhost":
-            PARAMETER.DB_HOST = arg
-        elif opt == "--dbport":
-            PARAMETER.DB_PORT = arg
-        elif opt == "--dbname":
-            PARAMETER.DB_NAME = arg
-        elif opt == "--dbuser":
-            PARAMETER.DB_USER = arg
-        elif opt == "--dbpassword":
-            PARAMETER.DB_PASSWORD = arg
-        elif opt == "--drop-table":
-            PARAMETER.DROP_TABLE = arg == 'y'
-
-    if PARAMETER.FILE_PATH is None:
-        print(__help())
-        sys.exit(2)
+        elif opt == get_long_option(PARAMETER_REDDIT_USERNAME):
+            PARAMETER.REDDIT_USERNAME = arg
+        elif opt == get_long_option(PARAMETER_REDDIT_CLIENT_ID):
+            PARAMETER.REDDIT_CLIENT_ID = arg
+        elif opt == get_long_option(PARAMETER_REDDIT_SECRET):
+            PARAMETER.REDDIT_SECRET = arg
+        # elif opt == "--table-prefix":
+        #     PARAMETER.TABLE_PREFIX = arg
+        # elif opt == "--dbhost":
+        #     PARAMETER.DB_HOST = arg
+        # elif opt == "--dbport":
+        #     PARAMETER.DB_PORT = arg
+        # elif opt == "--dbname":
+        #     PARAMETER.DB_NAME = arg
+        # elif opt == "--dbuser":
+        #     PARAMETER.DB_USER = arg
+        # elif opt == "--dbpassword":
+        #     PARAMETER.DB_PASSWORD = arg
+        # elif opt == "--drop-table":
+        #     PARAMETER.DROP_TABLE = arg == 'y'
 
     # check type value
-    # if (PARAMETER.TYPE is None or not PARAMETER.TYPE in PARAMETER.TYPE_AVAILABLE_VALUES):
-    #     print("No valid run-type = {} value!".format(PARAMETER.TYPE))
-    #     print(__help())
-    #     sys.exit(2)
+    check_existence_required_parameter(PARAMETER_REDDIT_USERNAME, PARAMETER.REDDIT_USERNAME)
+    check_existence_required_parameter(PARAMETER_REDDIT_CLIENT_ID, PARAMETER.REDDIT_CLIENT_ID)
+    check_existence_required_parameter(PARAMETER_REDDIT_SECRET, PARAMETER.REDDIT_SECRET)
 
     # global appType
     # appType = OUTPUT.getOutPutTypeByName(PARAMETER.TYPE)
 
-    log_console.info(os.linesep.join([
-        os.linesep
-        , "----------- trajectories-to-db configuration -----------: "
-        , "parameters values: "
-        , "--file-path:            csv file containing trajectories = " + PARAMETER.FILE_PATH
-        , "--chunk-size:           size of the chunk to export = " + str(PARAMETER.CHUNK_SIZE)
-        , "--table-prefix:         table prefix = " + PARAMETER.TABLE_PREFIX
-        , "--dbhost:               database host = " + PARAMETER.DB_HOST
-        , "--dbport:               database port = " + PARAMETER.DB_PORT
-        , "--dbname:               database name = " + PARAMETER.DB_NAME
-        , "--dbuser:               database user = " + PARAMETER.DB_USER
-        , "--dbpassword:           database password = " + PARAMETER.DB_PASSWORD
-        , "--drop-table:           drop tables = " + ('y' if PARAMETER.DROP_TABLE else 'n')
+    log.info("Start downloading subreddit")
 
-    ]))
+    # log_console.info(os.linesep.join([
+    #     os.linesep
+    #     , "----------- configuration -----------: "
+    #     , "parameters values: "
+    #     , "--file-path:            csv file containing trajectories = " + PARAMETER.FILE_PATH
+    #     , "--chunk-size:           size of the chunk to export = " + str(PARAMETER.CHUNK_SIZE)
+    #     , "--table-prefix:         table prefix = " + PARAMETER.TABLE_PREFIX
+    #     , "--dbhost:               database host = " + PARAMETER.DB_HOST
+    #     , "--dbport:               database port = " + PARAMETER.DB_PORT
+    #     , "--dbname:               database name = " + PARAMETER.DB_NAME
+    #     , "--dbuser:               database user = " + PARAMETER.DB_USER
+    #     , "--dbpassword:           database password = " + PARAMETER.DB_PASSWORD
+    #     , "--drop-table:           drop tables = " + ('y' if PARAMETER.DROP_TABLE else 'n')
+    #
+    # ]))
 
 
 def __help():
